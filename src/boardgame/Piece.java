8d5e5b32bcde08ca408b372cf43efe0fa5370c11
J-A-C-G <ja-carrerag@hotmail.com>
2019-08-05 18:33:35 -0500

    Starting to implement Board and Piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -3,18 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package application;
-
-import boardgame.Board;
+package boardgame;
 
 /**
  *
  * @author jaime
  */
-public class Program {
-
-    public static void main(String[] args) {
-        Board board= new Board(8, 8);
-    }
+public class Piece {
     
 }
